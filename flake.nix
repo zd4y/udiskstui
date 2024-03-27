47048ce29a48f01749845fbc66a818a86adad101
zd4y <59004538+zd4y@users.noreply.github.com>
2024-03-26 20:51:17 -0500

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
+        packages.default = naersk-lib.buildPackage ./.;
+        devShells.default = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
